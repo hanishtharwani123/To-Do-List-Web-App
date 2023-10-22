76a0090f7c6e03851175425ee1f909330bb470e1
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-10-22 20:55:05 +0530

    first commit

--- connect.php
+++ connect.php
@@ -4,8 +4,9 @@ $Email = $_POST['email'];
 $Password = $_POST['password'];
 $Number = $_POST['number'];
 
+
 // Database connection
-$conn = new mysqli('localhost', 'root', '', 'test');
+$conn = new mysqli("sql300.infinityfree.com", "if0_35281050", "5hKuMJfqYe5f" , 'if0_35281050_test');
 if ($conn->connect_error){
     die('Connection Failed : '.$conn->connect_error);
 }
@@ -15,15 +16,12 @@ else{
     $stmt->execute();
 
     if ($stmt->affected_rows > 0) {
-        // Registration was successful, so redirect to a new page
         header("Location: todoList.php");
-        exit; // Make sure to exit to prevent further execution
+        exit; 
     } else {
-        // Handle the case where registration is not successful
         echo "Registration Failed...";
     }
 
-    // Close the statement and the connection
     $stmt->close();
     $conn->close();
 }
